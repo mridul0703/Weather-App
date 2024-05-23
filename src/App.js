f0f2935fd1eab8cb31d8cf0262c8b0e04b45d0ea
Mridul M Kumar <98074317+mridul0703@users.noreply.github.com>
2024-05-23 23:18:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,10 +11,6 @@ function App() {
         <div style={{display : 'flex',justifyContent : 'center',overflowx: 'hidden'}}>
         <div className="weatherbody" id="weatherbody">
             <RowLeft />
-            <div className="rightcol">
-                <div className="card rcard no1"></div>
-                <div className="card rcard no2"></div>
-            </div>
         </div>
         </div>
         </>
